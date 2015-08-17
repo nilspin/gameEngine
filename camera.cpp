2eebbcf5173e9bbba68787c5ec6a9ade988e3181
Nilay Engineer <gooney28@gmail.com>
2015-08-17 20:17:29 +0530

    Completely rewrote camera class. See details.
    Completely rewrote the camera class to get it working with SDL.
    Mouse and keyboard ( WASD + left,right) work fine.

--- camera.cpp
+++ camera.cpp
@@ -1,209 +1,124 @@
-//camera.cpp
 #include "camera.h"
-using namespace std;
 
-Camera::Camera() {
-	camera_mode = FREE;
-	camera_up = glm::vec3(0, 1, 0);
-	field_of_view = 45;
-	rotation_quaternion = glm::quat(1, 0, 0, 0);
-	camera_position_delta = glm::vec3(0, 0, 0);
-	camera_scale = .5f;
-	max_pitch_rate = 5;
-	max_heading_rate = 5;
-	move_camera = false;
+glm::mat4 Camera::getViewMatrix(){
+	return ViewMatrix;
 }
-Camera::~Camera() {
+glm::mat4 Camera::getProjectionMatrix(){
+	return ProjectionMatrix;
 }
 
-void Camera::Reset() {
-	camera_up = glm::vec3(0, 1, 0);
+void Camera::rotate()
+{
+	static double lastTime = SDL_GetTicks();
+	double currentTime = SDL_GetTicks();
+	float deltaTime = float(currentTime - lastTime);
+	timeDifference = deltaTime;
+	// Get mouse position
+	int xpos = e.motion.xrel;
+	int ypos = e.motion.yrel;
+
+	// Compute new orientation
+	horizontalAngle += mouseSpeed * float(/*(1024 / 2) -*/ -xpos);
+	verticalAngle += mouseSpeed * float(/*(768 / 2) -*/ -ypos);
+
+	// For the next frame, the "last time" will be "now"
+	lastTime = currentTime;
+
+}
+void Camera::computeMatricesFromInputs(){
+
+	// glfwGetTime is called only once, the first time this function is called
+
+	// Compute time difference between current and last frame
+
 }
 
-void Camera::Update() {
-	camera_direction = glm::normalize(camera_look_at - camera_position);
-	//need to set the matrix state. this is only important because lighting doesn't work if this isn't done
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glViewport(viewport_x, viewport_y, window_width, window_height);
+void Camera::move(CameraDirection dir)
+{
+	switch (dir)
+	{
+	case FORWARD:	// Move forward
+		cout << "w pressed-" << endl;
+		position += direction * timeDifference * speed;
+		break;
 
-	if (camera_mode == ORTHO) {
-		//our projection matrix will be an orthogonal one in this case
-		//if the values are not floating point, this command does not work properly
-		//need to multiply by aspect!!! (otherise will not scale properly)
-		projection = glm::ortho(-1.5f * float(aspect), 1.5f * float(aspect), -1.5f, 1.5f, -10.0f, 10.f);
-	}
-	else if (camera_mode == FREE) {
-		projection = glm::perspective(field_of_view, aspect, near_clip, far_clip);
-		//detmine axis for pitch rotation
-		glm::vec3 axis = glm::cross(camera_direction, camera_up);
-		//compute quaternion for pitch based on the camera pitch angle
-		glm::quat pitch_quat = glm::angleAxis(camera_pitch, axis);
-		//determine heading quaternion from the camera up vector and the heading angle
-		glm::quat heading_quat = glm::angleAxis(camera_heading, camera_up);
-		//add the two quaternions
-		glm::quat temp = glm::cross(pitch_quat, heading_quat);
-		temp = glm::normalize(temp);
-		//update the direction from the quaternion
-		camera_direction = glm::rotate(temp, camera_direction);
-		//add the camera delta
-		camera_position += camera_position_delta;
-		//set the look at to be infront of the camera
-		camera_look_at = camera_position + camera_direction * 1.0f;
-		//damping for smooth camera
-		camera_heading *= .5;
-		camera_pitch *= .5;
-		camera_position_delta = camera_position_delta * .8f;
-	}
-	//compute the MVP
-	view = glm::lookAt(camera_position, camera_look_at, camera_up);
-	model = glm::mat4(1.0f);
-	MVP = projection * view * model;
-	glLoadMatrixf(glm::value_ptr(MVP));
-}
+	case BACK:		// Move backward
+		cout << "s pressed-" << endl;
+		position -= direction * timeDifference * speed;
+		break;
 
-//Setting Functions
-void Camera::SetMode(CameraType cam_mode) {
-	camera_mode = cam_mode;
-	camera_up = glm::vec3(0, 1, 0);
-	rotation_quaternion = glm::quat(1, 0, 0, 0);
+	case RIGHT:		// Move right
+		cout << "d pressed-" << endl;
+		position += right * timeDifference * speed;
+		break;
+
+	case LEFT:		// Move left
+		cout << "a pressed-" << endl;
+		position -= right * timeDifference * speed;
+		break;
+
+	case UP:		// Look up
+		verticalAngle += 0.1;
+		break;
+
+	case DOWN:
+		verticalAngle -= 0.1;
+		break;
+
+	case ROT_LEFT:
+		horizontalAngle += 0.1;
+		break;
+
+
+	case ROT_RIGHT:
+		horizontalAngle -= 0.1;
+		break;
+
+
+	}
+
 }
 
-void Camera::SetPosition(glm::vec3 pos) {
-	camera_position = pos;
+void Camera::SetPosition(glm::vec3 pos)
+{
+	position = InitialPos = pos;
 }
 
-void Camera::SetLookAt(glm::vec3 pos) {
-	camera_look_at = pos;
-}
-void Camera::SetFOV(double fov) {
-	field_of_view = fov;
-}
-void Camera::SetViewport(int loc_x, int loc_y, int width, int height) {
-	viewport_x = loc_x;
-	viewport_y = loc_y;
-	window_width = width;
-	window_height = height;
-	//need to use doubles division here, it will not work otherwise and it is possible to get a zero aspect ratio with integer rounding
-	aspect = double(width) / double(height);
-	;
-}
-void Camera::SetClipping(double near_clip_distance, double far_clip_distance) {
-	near_clip = near_clip_distance;
-	far_clip = far_clip_distance;
+void Camera::calcMatrices()
+{
+	// Direction : Spherical coordinates to Cartesian coordinates conversion
+	direction = glm::vec3(
+		cos(verticalAngle) * sin(horizontalAngle),
+		sin(verticalAngle),
+		cos(verticalAngle) * cos(horizontalAngle)
+		);
+
+	// Right vector
+	right = glm::vec3(
+		sin(horizontalAngle - 3.14f / 2.0f),
+		0,
+		cos(horizontalAngle - 3.14f / 2.0f)
+		);
+
+	// Up vector
+	up = glm::cross(right, direction);
+
+	float FoV = initialFoV;
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Camera matrix
+	ViewMatrix = glm::lookAt(
+		position,           // Camera is here
+		position + direction, // and looks here : at the same position, plus "direction"
+		up                  // Head is up (set to 0,-1,0 to look upside-down)
+		);
 }
 
-void Camera::Move(CameraDirection dir) {
-	if (camera_mode == FREE) {
-		switch (dir) {
-		case UP:
-			camera_position_delta += camera_up * camera_scale;
-			break;
-		case DOWN:
-			camera_position_delta -= camera_up * camera_scale;
-			break;
-		case LEFT:
-			camera_position_delta -= glm::cross(camera_direction, camera_up) * camera_scale;
-			break;
-		case RIGHT:
-			camera_position_delta += glm::cross(camera_direction, camera_up) * camera_scale;
-			break;
-		case FORWARD:
-			camera_position_delta += camera_direction * camera_scale;
-			break;
-		case BACK:
-			camera_position_delta -= camera_direction * camera_scale;
-			break;
-		}
-	}
-}
-void Camera::ChangePitch(float degrees) {
-	//Check bounds with the max pitch rate so that we aren't moving too fast
-	if (degrees < -max_pitch_rate) {
-		degrees = -max_pitch_rate;
-	}
-	else if (degrees > max_pitch_rate) {
-		degrees = max_pitch_rate;
-	}
-	camera_pitch += degrees;
-
-	//Check bounds for the camera pitch
-	if (camera_pitch > 360.0f) {
-		camera_pitch -= 360.0f;
-	}
-	else if (camera_pitch < -360.0f) {
-		camera_pitch += 360.0f;
-	}
-}
-void Camera::ChangeHeading(float degrees) {
-	//Check bounds with the max heading rate so that we aren't moving too fast
-	if (degrees < -max_heading_rate) {
-		degrees = -max_heading_rate;
-	}
-	else if (degrees > max_heading_rate) {
-		degrees = max_heading_rate;
-	}
-	//This controls how the heading is changed if the camera is pointed straight up or down
-	//The heading delta direction changes
-	if (camera_pitch > 90 && camera_pitch < 270 || (camera_pitch < -90 && camera_pitch > -270)) {
-		camera_heading -= degrees;
-	}
-	else {
-		camera_heading += degrees;
-	}
-	//Check bounds for the camera heading
-	if (camera_heading > 360.0f) {
-		camera_heading -= 360.0f;
-	}
-	else if (camera_heading < -360.0f) {
-		camera_heading += 360.0f;
-	}
-}
-void Camera::Move2D(int x, int y) {
-	//compute the mouse delta from the previous mouse position
-	glm::vec3 mouse_delta = mouse_position - glm::vec3(x, y, 0);
-	//if the camera is moving, meaning that the mouse was clicked and dragged, change the pitch and heading
-	if (move_camera) {
-		ChangeHeading(.08f * mouse_delta.x);
-		ChangePitch(.08f * mouse_delta.y);
-	}
-	mouse_position = glm::vec3(x, y, 0);
-}
-
-void Camera::SetPos(Uint8 button, Uint8 state, int x, int y) {
-	if (button == SDL_BUTTON_LEFT && state == SDL_PRESSED) {
-		camera_position_delta += camera_up * .05f;
-		std::cout << (Uint8)button << " pressed \n";
-	}
-	else if (button == SDL_BUTTON_RIGHT && state == SDL_PRESSED) {
-		camera_position_delta -= camera_up * .05f;
-		std::cout << (Uint8)button << " pressed \n";
-	}
-	else if (button == SDL_BUTTON_LEFT && state == SDL_RELEASED) {
-		std::cout << (Uint8)button << " released \n";
-		move_camera = true;
-	}
-	else if (button == SDL_BUTTON_RIGHT && state == SDL_RELEASED) {
-		std::cout << (Uint8)button << " released \n";
-		move_camera = false;
-	}
-	mouse_position = glm::vec3(x, y, 0);
-	
-}
-
-CameraType Camera::GetMode() {
-	return camera_mode;
-}
-
-void Camera::GetViewport(int &loc_x, int &loc_y, int &width, int &height) {
-	loc_x = viewport_x;
-	loc_y = viewport_y;
-	width = window_width;
-	height = window_height;
-}
-
-void Camera::GetMatricies(glm::mat4 &P, glm::mat4 &V, glm::mat4 &M) {
-	P = projection;
-	V = view;
-	M = model;
+void Camera::Reset()
+{
+	position = InitialPos;
+	horizontalAngle = 3.14f;
+	verticalAngle = 0.0f;
+	calcMatrices();
 }
--- camera.h
+++ camera.h
@@ -1,107 +1,64 @@
 /*
 camera.h
 OpenGL Camera Code
-Capable of 2 modes, orthogonal, and free
-Quaternion camera code adapted from : http ://hamelot.co.uk/visualization/opengl-camera/
-Written by Hammad Mazhar
 */
 #ifndef CAMERA_H
 #define CAMERA_H
 
 #include "stdafx.h"
 #include <glm.hpp>
-#include <gtx/transform.hpp>
-#include <gtc/quaternion.hpp>
-#include <gtx/quaternion.hpp>
 #include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
+#include <SDL.h>
 
-enum CameraType {
-	ORTHO, FREE
-};
+extern SDL_Window* window;
+extern SDL_Event e;
+
+using namespace std;
+
 enum CameraDirection {
-	UP, DOWN, LEFT, RIGHT, FORWARD, BACK
+	LEFT, RIGHT, FORWARD, BACK, UP, DOWN, ROT_LEFT, ROT_RIGHT
 };
 
 class Camera {
+private :
+
+	glm::mat4 ViewMatrix;
+	glm::mat4 ProjectionMatrix;
+
+	// Initial position : on +Z
+	glm::vec3 position,InitialPos;
+	// Initial horizontal angle : toward -Z
+	float horizontalAngle = 3.14f;
+	// Initial vertical angle : none
+	float verticalAngle = 0.0f;
+	// Initial Field of View
+	float initialFoV = 45.0f;
+	// Actual direction
+	glm::vec3 direction;
+	// Right vector
+	glm::vec3 right;
+	// Up
+	glm::vec3 up;
+	// Time difference
+	float timeDifference = 0;
+	
+	float speed = 0.2f; // 3 units / second
+	float mouseSpeed = 0.02f;
+
+	int temp = 0;
+
 public:
-	Camera();
-	~Camera();
-
+//	Camera();
+//	~Camera();
+	void SetPosition(glm::vec3);
+	void move(CameraDirection);
+	void computeMatricesFromInputs();
+	glm::mat4 getViewMatrix();
+	glm::mat4 getProjectionMatrix();
 	void Reset();
-	//This function updates the camera
-	//Depending on the current camera mode, the projection and viewport matricies are computed
-	//Then the position and location of the camera is updated
-	void Update();
-
-	//Given a specific moving direction, the camera will be moved in the appropriate direction
-	//For a spherical camera this will be around the look_at point
-	//For a free camera a delta will be computed for the direction of movement.
-	void Move(CameraDirection dir);
-	//Change the pitch (up, down) for the free camera
-	void ChangePitch(float degrees);
-	//Change heading (left, right) for the free camera
-	void ChangeHeading(float degrees);
-
-	//Change the heading and pitch of the camera based on the 2d movement of the mouse
-	void Move2D(int x, int y);
-
-	//Setting Functions
-	//Changes the camera mode, only three valid modes, Ortho, Free, and Spherical
-	void SetMode(CameraType cam_mode);
-	//Set the position of the camera
-	void SetPosition(glm::vec3 pos);
-	//Set's the look at point for the camera
-	void SetLookAt(glm::vec3 pos);
-	//Changes the Field of View (FOV) for the camera
-	void SetFOV(double fov);
-	//Change the viewport location and size
-	void SetViewport(int loc_x, int loc_y, int width, int height);
-	//Change the clipping distance for the camera
-	void SetClipping(double near_clip_distance, double far_clip_distance);
-
-	void SetDistance(double cam_dist);
-	void SetPos(Uint8 button, Uint8 state, int x, int y);
-
-	//Getting Functions
-	CameraType GetMode();
-	void GetViewport(int &loc_x, int &loc_y, int &width, int &height);
-	void GetMatricies(glm::mat4 &P, glm::mat4 &V, glm::mat4 &M);
-
-	CameraType camera_mode;
-
-	int viewport_x;
-	int viewport_y;
-
-	int window_width;
-	int window_height;
-
-	double aspect;
-	double field_of_view;
-	double near_clip;
-	double far_clip;
-
-	float camera_scale;
-	float camera_heading;
-	float camera_pitch;
-
-	float max_pitch_rate;
-	float max_heading_rate;
-	bool move_camera;
-
-	glm::vec3 camera_position;
-	glm::vec3 camera_position_delta;
-	glm::vec3 camera_look_at;
-	glm::vec3 camera_direction;
-
-	glm::vec3 camera_up;
-	glm::quat rotation_quaternion;
-	glm::vec3 mouse_position;
-
-	glm::mat4 projection;
-	glm::mat4 view;
-	glm::mat4 model;
-	glm::mat4 MVP;
+	void rotate();
+	void calcMatrices();
+	void s(int i){ temp += i; }
 
 };
 #endif
